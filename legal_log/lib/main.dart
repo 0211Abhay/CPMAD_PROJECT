cab0ad76520091218c0b25f28c686564759b05e7
0211Abhay <abhaynathwani123@gmail.com>
2024-11-12 19:33:44 +0530

    added dependencies for Stage 1

--- legal_log/lib/main.dart
+++ legal_log/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:legal_log/screens/homeScreen.dart';
 import 'package:legal_log/screens/splash_screen.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value){
+    print('Firebase app initialized successfully');
+  });
   runApp(MaterialApp(
     home: SplashScreen(),
     routes: {
