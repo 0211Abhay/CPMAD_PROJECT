ae671d570572001bd5d970dba27194824ff7c44b
0211Abhay <abhaynathwani123@gmail.com>
2024-12-21 16:44:59 +0530

    file upload , fetch list and view completed need to add file delete

--- legal_log/macos/Flutter/GeneratedPluginRegistrant.swift
+++ legal_log/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import geolocator_apple
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import sqflite_darwin
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
