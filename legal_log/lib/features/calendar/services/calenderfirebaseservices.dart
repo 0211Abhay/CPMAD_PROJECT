ebe5aa7d2f11cdceb085de4004ac253ef5a76c65
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2024-12-30 07:59:13 +0530

    Added Fetch History based on user login

--- legal_log/lib/features/calendar/services/calenderfirebaseservices.dart
+++ legal_log/lib/features/calendar/services/calenderfirebaseservices.dart
@@ -1,18 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_storage/get_storage.dart';
 
 class CalendarCaseFirebaseServices {
   final FirebaseFirestore db = FirebaseFirestore.instance;
-
+  final GetStorage storage = GetStorage();
   Future<List<Map<String, dynamic>>> fetchCasesForToday(DateTime date) async {
     // Get the start and end of the day
     DateTime startOfDay = DateTime(date.year, date.month, date.day).toUtc();
     DateTime endOfDay = startOfDay.add(Duration(days: 1)).toUtc();
+
+    // Fetch the advocate_id from storage
+    var user = storage.read('user');
+    var advocateId = user != null ? user['advocate_id'] : null;
+
+    print("Current advocate_id: $advocateId");
 
     try {
       QuerySnapshot querySnapshot = await db
           .collection('case_history') // Replace with your collection name
           .where('next_date', isGreaterThanOrEqualTo: startOfDay)
           .where('next_date', isLessThan: endOfDay)
+          .where('advocate_id', isEqualTo: advocateId)
           .get();
 
       List<Map<String, dynamic>> cases = querySnapshot.docs.map((document) {
